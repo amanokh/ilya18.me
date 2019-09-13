b11ffcaa641da34a59be54cf1ffc030fed6651b0
Anton Manokhin <a.manokh@gmail.com>
2019-09-13 20:08:49 +0300

    Animations test

--- js.js
+++ js.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
     if (cookie == "true") {
         window.location.href="/quiz";
     }
+    var body = $('html')
+    setTimeout(function() {body.css({'display':'unset','animation':'fadeInDown 0.5s cubic-bezier(.36,.07,.19,.97) both'});}, 500);
 });
 function getCookie(name) {
   let matches = document.cookie.match(new RegExp(
