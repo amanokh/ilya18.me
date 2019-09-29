ffa26dd88689fd2e5ea7633548302b0bcf0e6f7b
Anton Manokhin <a.manokh@gmail.com>
2019-09-29 12:39:16 +0300

    Gifts

---
+++ scripts/gifts_irk.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    var cookie = getCookie("userLogged");
+    if (cookie != "true") {
+        window.location.href="/";
+    }
+    var body = $('html');
+    setTimeout(function() {body.css({'display':'unset','animation':'fadeInDown 0.5s cubic-bezier(.36,.07,.19,.97) both'});}, 200);
+});
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
