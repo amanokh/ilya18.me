9e733ab97786cd69f7febd355549589c6ee1022e
Anton Manokhin <a.manokh@gmail.com>
2019-10-04 13:41:42 +0300

    Update gifts_irk.js

--- scripts/gifts_irk.js
+++ scripts/gifts_irk.js
@@ -1,14 +1,4 @@
 $(document).ready(function() {
-    var cookie = getCookie("userLogged");
-    if (cookie != "true") {
-        window.location.href="/";
-    } else {
-        var pathName = window.location.pathname;
-        var curStage = getCookie("curStage");
-        if (curStage < 10) {
-            window.location.href=(atob(stages[curStage]));
-        }
-    }
     var body = $('html');
     setTimeout(function() {body.css({'display':'unset','animation':'fadeInDown 0.4s cubic-bezier(.36,.07,.19,.97) both'});}, 200);
 });
